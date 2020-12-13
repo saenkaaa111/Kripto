dc10225de3f3bfc5109b159ff88d602cfa2e0133
saenkaaa111 <72892545+saenkaaa111@users.noreply.github.com>
2020-12-13 18:08:00 +0300

    Add files via upload

---
+++ lr2/var7/__init__.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
+from flask_mail import Mail, Message
+import os
+
+db = SQLAlchemy()
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'secret-key-goes-here'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME', 'email@example.com') 
+app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER', '"" <noreply@example.com>')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD', 'password')
+
+
+db.init_app(app)
+mail = Mail(app)
+
+login_manager = LoginManager()
+login_manager.login_view = 'main.login'
+login_manager.init_app(app)
+
+from .models import User
+
+@login_manager.user_loader
+def load_user(user_id):
+    return user.query.get(int(user_id))
+
+bootstrap = Bootstrap(app)
+
+from .main import main as main_blueprint
+app.register_blueprint(main_blueprint)
